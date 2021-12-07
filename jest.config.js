ebf9b3df2adca8482c3ab7c4991b971c385aa411
cfrinka <c.frinkaneto@gmail.com>
2021-12-07 12:14:26 -0300

    updates libs

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/ngshop',
     '<rootDir>/apps/admin',
     '<rootDir>/libs/ui',
     '<rootDir>/libs/orders',
     '<rootDir>/libs/products',
-    '<rootDir>/libs/users',
-  ],
+    '<rootDir>/libs/users'
+  ]
 };
